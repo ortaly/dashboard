0121a7019f09ab2f862555c7f4f06def92df3a95
ortal <ortal@tikalk.com>
2014-11-25 14:43:40 +0200

    dashboard main page

--- app/scripts/controllers/topnavigationbar.js
+++ app/scripts/controllers/topnavigationbar.js
@@ -9,34 +9,33 @@
  */
 angular.module('angularDashboardApp')
   .controller('TopnavigationbarCtrl', ['$scope', 'TopNavDataService', function ($scope, TopNavDataService) {
-  	$scope.name = '';
   	TopNavDataService.getDashboardName().then(function(res){
-  		$scope.name = res;
-  	}
-  	,function(reason){
-		console.log(reason);
-	});
+  		  $scope.name = res;
+    	}
+    	,function(reason){
+  		console.log(reason);
+  	});
 
-	TopNavDataService.getMessages().then(function(res){
-  		$scope.messages = res;
-  	}
-  	,function(reason){
-		console.log(reason);
-	});
+  	TopNavDataService.getMessages().then(function(res){
+    		$scope.messages = res;
+    	}
+    	,function(reason){
+  		console.log(reason);
+  	});
 
-	TopNavDataService.getTasks().then(function(res){
-  		$scope.tasks = res;
-  	}
-  	,function(reason){
-		console.log(reason);
-	});
+  	TopNavDataService.getTasks().then(function(res){
+    		$scope.tasks = res;
+    	}
+    	,function(reason){
+  		console.log(reason);
+  	});
 
-	TopNavDataService.getAlerts().then(function(res){
-  		$scope.alerts = res;
-  	}
-  	,function(reason){
-		console.log(reason);
-	});
+  	TopNavDataService.getAlerts().then(function(res){
+    		$scope.alerts = res;
+    	}
+    	,function(reason){
+  		console.log(reason);
+  	});
   }]).
   service('TopNavDataService', ['$http', '$q', function($http, $q){
   	this.getDashboardName = function(){
@@ -49,38 +48,39 @@ angular.module('angularDashboardApp')
   	this.getMessages = function(){
   		var deferred = $q.defer();
   		deferred.resolve([
-			{name:'John Smith', received:'Yesterday', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...'},
-			{name:'Tal Cohen', received:'11/11/14', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...'},
-			{name:'Lital Dahan', received:'10/11/14', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...'}
-		]);
+  			{name:'John Smith', received:'Yesterday', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...'},
+  			{name:'Tal Cohen', received:'11/11/14', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...'},
+  			{name:'Lital Dahan', received:'10/11/14', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...'}
+  		]);
 
-		return deferred.promise;
+		  return deferred.promise;
 
   	}
 
   	this.getTasks = function(){
   		var deferred = $q.defer();
   		deferred.resolve([
-			{name: 'task 1', complete:'40', class:'progress-bar-success'},
-			{name: 'task 2', complete:'20', class:'progress-bar-info'},
-			{name: 'task 3', complete:'60', class:'progress-bar-warning'},
-			{name: 'task 4', complete:'80', class:'progress-bar-danger'}
-		]);
+  			{name: 'task 1', complete:'40', class:'progress-bar-success'},
+  			{name: 'task 2', complete:'20', class:'progress-bar-info'},
+  			{name: 'task 3', complete:'60', class:'progress-bar-warning'},
+  			{name: 'task 4', complete:'80', class:'progress-bar-danger'}
+  		]);
 
-		return deferred.promise;
+  		return deferred.promise;
 
   	}
 
   	this.getAlerts = function(){
   		var deferred = $q.defer();
   		deferred.resolve([
-			{name: 'New Comment', received:'4 minutes ago', class:'fa-comment'},
-			{name: 'New Followers', received:'12 minutes ago', class:'fa-twitter'},
-			{name: 'Message Sent', received:'4 minutes ago', class:'fa-envelope'},
-			{name: 'New Task', received:'4 minutes ago', class:'fa-tasks'},
-			{name: 'Server Rebooted', received:'4 minutes ago', class:'fa-upload'}
-		]);
-		return deferred.promise;
+  			{name: 'New Comment', received:'4 minutes ago', class:'fa-comment'},
+  			{name: 'New Followers', received:'12 minutes ago', class:'fa-twitter'},
+  			{name: 'Message Sent', received:'4 minutes ago', class:'fa-envelope'},
+  			{name: 'New Task', received:'4 minutes ago', class:'fa-tasks'},
+  			{name: 'Server Rebooted', received:'4 minutes ago', class:'fa-upload'}
+  		]);
+
+  		return deferred.promise;
 
   	}
 
